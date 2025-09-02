e26e4406ffd6a3367a2607a61e3445259dbe5b61
Benoît Legat <benoit.legat@gmail.com>
2025-09-02 16:34:21 +0200

    Switch to TestItems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,3 @@
-using Test
+using TestItemRunner
 
-for file in readdir(@__DIR__)
-    if file in ["runtests.jl"]
-        continue
-    elseif !endswith(file, ".jl")
-        continue
-    end
-    @testset "$(file)" begin
-        include(file)
-    end
-end
+@run_package_tests
